b2774fdd3653155a41b8923bcf248775d83039d8
Jan Mischak <jan.mischak@kstp.at>
2020-12-06 03:33:54 +0100

    Update foo test gui

--- gui/foo.lua
+++ gui/foo.lua
@@ -2,46 +2,46 @@
 local gui = require("gui-plus")
 
 return gui.register_class{
-  class_name = "foo",
+  name = "foo",
 
-  structure = {
-    type = "frame",
-    direction = "vertical",
-    children = {
-      {
-        type = "label",
-        name = "label",
-        caption = gui.dynamic{
-          on = "state.count",
-          setter = function(state) return "count: " .. state.count end,
-        },
+  type = "frame",
+  direction = "vertical",
+  children = {
+    {
+      type = "label",
+      name = "label",
+      caption = gui.dynamic{
+        on = "state.count",
+        set = function(scopes, state)
+          return "count: " .. state.count
+        end,
       },
-      {
-        type = "flow",
-        name = "flow",
-        direction = "horizontal",
-        children = {
-          {
-            type = "button",
-            name = "decr",
-            caption = "decr",
-            events = {
-              on_click = function(event, state)
-                state.count = state.count - 1
-                gui.redraw(state)
-              end,
-            },
+    },
+    {
+      type = "flow",
+      name = "flow",
+      direction = "horizontal",
+      children = {
+        {
+          type = "button",
+          name = "decr",
+          caption = "decr",
+          events = {
+            on_click = function(event, scopes, state)
+              state.count = state.count - 1
+              gui.redraw(state)
+            end,
           },
-          {
-            type = "button",
-            name = "incr",
-            caption = "incr",
-            events = {
-              on_click = function(event, state)
-                state.count = state.count + 1
-                gui.redraw(state)
-              end,
-            },
+        },
+        {
+          type = "button",
+          name = "incr",
+          caption = "incr",
+          events = {
+            on_click = function(event, scopes, state)
+              state.count = state.count + 1
+              gui.redraw(state)
+            end,
           },
         },
       },

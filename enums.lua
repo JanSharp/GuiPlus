7f994a0842f292ac93891957faeae23eda9eaf7e
Jan Mischak <jan.mischak@kstp.at>
2021-01-08 22:06:26 +0100

    Move enum debug hooking to JanSharpDevEnv

--- enums.lua
+++ enums.lua
@@ -18,38 +18,10 @@ local enums = {
   },
 }
 
--- replace numbers with tables with metamethods to display
--- enum names instead of raw numbers
-if __DebugAdapter then
-  __DebugAdapter.defineGlobal("enums")
-  _ENV["enums"..""] = enums -- for the variables view and debug console
-  -- concat for it to not be considered a defined global
-
-  local lookups = {}
-
-  local meta = {
-    __debugline = function(enum_value)
-      local enum_name = enum_value.__enum_name
-      return "enums." .. enum_name .. "." .. lookups[enum_name][enum_value.__value]
-    end,
-    __debugchildren = false,
-    __debugtype = "number",
-  }
-
-  for enum_name, values in next, enums do
-    local lookup = {}
-    for k, v in pairs(values) do
-      lookup[v] = k
-    end
-    lookups[enum_name] = lookup
-    for value_name, value in next, values do
-      local new_value = {
-        __enum_name = enum_name,
-        __value = value,
-      }
-      values[value_name] = setmetatable(new_value, meta)
-    end
-  end
+-- replaces numbers with tables with metamethods to display enum names instead of raw numbers
+-- puts the enums_table in global to make it accessible in the variables view and debug console
+if __DebugAdapter and script.active_mods["JanSharpDevEnv"] then
+  require("__JanSharpDevEnv__.enum-debug-util").hook_enums(enums, "enums")
 end
 
 return enums
